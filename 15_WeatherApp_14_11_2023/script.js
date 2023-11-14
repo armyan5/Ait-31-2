39432d18c26b61b3bc5fda150813714a87567a5b
Arman <arman-hakobyan@gmx.de>
2023-11-14 18:01:31 +0100

    new

---
+++ 15_WeatherApp_14_11_2023/script.js
@@ -0,0 +1,34 @@
+const API_KEY = '1d2ec45002958ddc80002f2f7f1c4cc7';
+
+const input = document.getElementById('locationInput');
+const button = document.getElementById('getWeatherButton');
+const info = document.getElementById('WeatherContainer');
+
+
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric
+
+button.onclick = () => {
+    const cityName = input.value.trim();
+
+    if(cityName) {
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
+        .then(response => response.json())
+        .then(weather => displayWeather(weather));
+    }
+    input.value = '';
+};
+
+function displayWeather(data) {
+
+const {name, main: {temp}, weather: [{description}], wind: {speed}} = data;
+
+info.innerHTML = `
+    <h2>${name}</h2>
+    <p>Temperature: ${temp}℃</p>
+    <p>Description: ${description}</p>
+    <p>wind speed: ${speed}m/s</p>`
+
+
+
+};
