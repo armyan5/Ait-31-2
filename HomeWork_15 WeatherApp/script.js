93a72115b9e412ded686cb77c0a6701ce10af4ea
Arman <arman-hakobyan@gmx.de>
2023-11-15 11:16:45 +0100

    update

--- HomeWork_15 WeatherApp/script.js
+++ HomeWork_15 WeatherApp/script.js
@@ -7,27 +7,51 @@ const info = document.getElementById('WeatherContainer');
 
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric
+//https://openweathermap.org/img/wn/04n@2x.png
 
-button.onclick = () => {
+button.onclick = async() => {
     const cityName = input.value.trim();
 
     if(cityName) {
-     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
-        .then(response => response.json())
-        .then(weather => displayWeather(weather));
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&lang=de&units=metric`);
+            const weatherInfo = await response.json();
+            input.value = '';
+            displayWeather(weatherInfo);
+        }catch(error) {
+            console.log(error);
+        }
+    
     }
-    input.value = '';
+   
 };
 
 function displayWeather(data) {
+    console.log(data);
 
-const {name, main: {temp}, weather: [{description}], wind: {speed}} = data;
+const {name,
+    clouds:{all},
+    sys: {country,sunrise,sunset},
+    visibility,
+    coord:{lon, lat},
+    main: {temp, feels_like, temp_min, temp_max, pressure, humidity}, 
+    weather: [{description,icon}], 
+    wind: {speed}
+} = data;
+
+
 
 
 info.innerHTML = `
-    <h2>${name} ✈</h2>
-    <p>Temperature: ${temp}℃</p>
-    <p>Description: ${description} </p>
-    <p>wind speed: ${speed}m/s</p>`
+    <h2><img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="icon">${name} ✈,${country}</h2>
+    <p> Temperature: ${temp}℃, Feels like: ${feels_like}</p>
+    <p>Max Temperature: ${temp_max}℃, Min temperature: ${temp_min}℃</p>
+    <p>Description: ${description},clouds:${all}%,pressure: ${pressure},humidity: ${humidity}</p>
+    <p>Coord: ${lon},${lat} </p>
+    <p>wind speed: ${speed}m/s</p>
+    <p>wind Visibility: ${visibility/1000}km</p>
+    <p>wind Sunrise: ${sunrise}, Sunset: ${sunset}</p>
+
+    `
     
 };
