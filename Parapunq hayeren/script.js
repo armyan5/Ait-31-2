a905cb9d38b1ace99e1b508a0eafc8ea3ba4ed77
Arman <arman-hakobyan@gmx.de>
2023-11-27 09:25:41 +0100

    neu

--- Parapunq hayeren/script.js
+++ Parapunq hayeren/script.js
@@ -28,8 +28,66 @@ if (x === "good") {
     
 }else {
     alert("qez inch exav axper jan?");
-}
+};
+
+
+
+let a = {
+    name: "Joe",
+    age: 25,
+    phone: "8913748932"
+
+};
+
+// Object.keys(a).forEach(function(key) {
+//     let value = a[key];
+//     alert(key + ":" + value )    // dursenq hanum a i meji keyn u valuen u tpumenq
+// });
+
+// alert(Object.values(a)); arandzin alert enq anum mer a i valuen
 
 
 
 
+
+// let stringify = function(obj) {
+//     let result = "";
+
+//     Object.keys(obj).forEach(function(key){      //  stringify vazuma mer objekti vrov  sarquma tox 
+//         let value = obj[key];
+//         result += key + ":" + value + "\n";
+
+//     });
+
+//     return result;
+// };
+
+let str = JSON.stringify(a);    // JSON!!!!!!1
+let b = JSON.parse(str);
+alert(b.name);
+
+
+// let z = function () {
+//     alert("Hi Poxosik") // funkcia hayatararelu dzev
+    
+// };
+
+// z();
+
+let y = 10;
+
+let div = function(t, s) {
+    return t / s;
+};
+
+let dip = function(y, zoo) { // 4, yup
+    return zoo(y) * zoo(y);
+};
+
+let yup = function(c) {
+    return div(y,2);
+};
+
+let d = dip(4, yup);
+
+alert(d);
